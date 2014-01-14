447d0538f4e6d6671dbe42050a263e80b335b350
Christophe Cossou <ccossou@gmail.com>
2014-01-14 14:53:52 +0100

    adding a module to define simple and double precision real

---
+++ numerical_types.f90
@@ -0,0 +1,21 @@
+!******************************************************************************
+! MODULE: numerical_types
+!******************************************************************************
+!
+! DESCRIPTION: 
+!> @brief Module that define simple and double precision parameters. 
+!
+!> @example
+!! real(sp) :: a ! real simple precision
+!! real(dp) :: b ! real double precision
+!
+!******************************************************************************
+
+module numerical_types
+
+  implicit none
+  
+  integer, parameter :: sp = selected_real_kind(6,37) !< @param simple precision parameter (used with real(sp) :: arg)
+  integer, parameter :: dp = selected_real_kind(15,307) !< @param double precision parameter (used with real(dp) :: arg)
+
+end module numerical_types
