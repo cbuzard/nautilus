e7026a75420ebe3a739ce51db16106edfc1fac5e
Christophe Cossou <ccossou@gmail.com>
2014-01-17 16:24:24 +0100

    adding a mainpage.txt file that store a presentation of the code that is added to the Doxygen documentation. Still need to be improved, of course.

--- iso_fortran_env.f90
+++ iso_fortran_env.f90
@@ -16,12 +16,12 @@ module iso_fortran_env
   public 
 
   integer, parameter :: Character_Storage_Size = 8 
-  integer, parameter :: Error_Unit = 0 
+  integer, parameter :: Error_Unit = 0 !< unit to print in the standard error, to be used : write(Error_Unit, *) "Error 404 bla bla bla"
   integer, parameter :: File_Storage_Size = 8 
-  integer, parameter :: Input_Unit = 5 
+  integer, parameter :: Input_Unit = 5  !< Input unit integer
   integer, parameter :: IOSTAT_END = -1 
   integer, parameter :: IOSTAT_EOR = -2 
   integer, parameter :: Numeric_Storage_Size = 32 
-  integer, parameter :: Output_Unit = 6 
+  integer, parameter :: Output_Unit = 6 !< unit to print in the standard output to be used as : write(Output_Unit, *) "Hello World !"
 
 end module iso_fortran_env
--- numerical_types.f90
+++ numerical_types.f90
@@ -6,8 +6,8 @@
 !> @brief Module that define simple and double precision parameters. 
 !
 !> @example
-!! real(sp) :: a ! real simple precision
-!! real(dp) :: b ! real double precision
+!!\n real(sp) :: a ! real simple precision
+!!\n real(dp) :: b ! real double precision
 !
 !******************************************************************************
 
@@ -15,7 +15,7 @@ module numerical_types
 
   implicit none
   
-  integer, parameter :: simple_precision = selected_real_kind(6,37) !< @param simple precision parameter (used with real(sp) :: arg)
-  integer, parameter :: double_precision = selected_real_kind(15,307) !< @param double precision parameter (used with real(dp) :: arg)
+  integer, parameter :: simple_precision = selected_real_kind(6,37) !< simple precision parameter (used with real(sp) :: arg)
+  integer, parameter :: double_precision = selected_real_kind(15,307) !< double precision parameter (used with real(dp) :: arg)
 
 end module numerical_types
