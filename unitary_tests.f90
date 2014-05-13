79437cd4252da159f4f4f63e7070e9e587013f32
Christophe Cossou <ccossou@gmail.com>
2014-05-13 15:37:12 +0200

    correcting a few bugs on unitary tests about time evolution

--- unitary_tests.f90
+++ unitary_tests.f90
@@ -21,7 +21,6 @@ program unitary_tests
   use iso_fortran_env, only : error_unit
   use global_variables
   use nautilus_main
-!~   use structure
 
     
   implicit none
@@ -30,6 +29,7 @@ program unitary_tests
   
   call initialisation()
   
+  call test_read_time_evolution()
   call test_time_interpolation()
   
   contains
@@ -91,7 +91,7 @@ program unitary_tests
     
     call init_structure_evolution()
     
-    open(10, file='time_interpolation.dat')
+    open(10, file='test_time_interpolation.dat')
     write(10,*) '# time, av, density, gas_temperature, grain_temperature'
     do j=1,nb_sample
       time = t_min * step**(j - 1.d0)
@@ -111,7 +111,7 @@ program unitary_tests
     open(12, file="gas_temperature_interpolation.gnuplot")
     open(13, file="grain_temperature_interpolation.gnuplot")
     
-    do j=10,11
+    do j=10,13
       write(j,*) "set terminal pdfcairo enhanced"
     end do
     
@@ -140,21 +140,154 @@ program unitary_tests
       write(j,*) 'set logscale x'
     end do
 
-    write(10,*) 'plot "time_interpolation.dat" using 1:2 with points linetype 1 pointtype 2 title "Interpolation",\'
+    write(10,*) 'plot "test_time_interpolation.dat" using 1:2 with points linetype 1 pointtype 2 title "Interpolation",\'
     write(10,*) '     "structure_evolution.dat" using 1:(10**$2) with lines title "Profile"'
 
-    write(11,*) 'plot "time_interpolation.dat" using 1:3 with points linetype 1 pointtype 2 title "Interpolation",\'
+    write(11,*) 'plot "test_time_interpolation.dat" using 1:3 with points linetype 1 pointtype 2 title "Interpolation",\'
     write(11,*) '     "structure_evolution.dat" using 1:(10**$3) with lines title "Profile"'
 
-    write(12,*) 'plot "time_interpolation.dat" using 1:4 with points linetype 1 pointtype 2 title "Interpolation",\'
+    write(12,*) 'plot "test_time_interpolation.dat" using 1:4 with points linetype 1 pointtype 2 title "Interpolation",\'
     write(12,*) '     "structure_evolution.dat" using 1:(10**$4) with lines title "Profile"'
 
-    write(12,*) 'plot "time_interpolation.dat" using 1:5 with points linetype 1 pointtype 2 title "Interpolation",\'
-    write(12,*) '     "structure_evolution.dat" using 1:(10**$5) with lines title "Profile"'
+    write(13,*) 'plot "test_time_interpolation.dat" using 1:5 with points linetype 1 pointtype 2 title "Interpolation",\'
+    write(13,*) '     "structure_evolution.dat" using 1:(10**$5) with lines title "Profile"'
     
     close(10)
     close(11)
+    close(12)
+    close(13)
   
   end subroutine test_time_interpolation
+
+!%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+!> @author 
+!> Christophe Cossou
+!
+!> @date 13 may 2014
+!
+! DESCRIPTION: 
+!> @brief Test the reading of time evolution, compare the actual profile 
+!! with the read one (in global variable)
+!
+!%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 
+  subroutine test_read_time_evolution()
+  
+    implicit none
+    
+    integer, parameter :: nb_sample = 100
+    real(double_precision), parameter :: t_min = 0.1d0 ! in AU
+    real(double_precision), parameter :: t_max = 100.d0! in AU
+    real(double_precision), parameter :: step = (t_max / t_min)**(1.d0 / (dfloat(nb_sample)))
+    
+    real(double_precision) :: time ! Time in Myr
+    real(double_precision) :: av, density, gas_temperature, grain_temperature
+
+    logical :: isDefined !< true or false, to test if some files exists or not, for instance
+    
+    integer :: j ! for loops
+    
+    write(*,*) 'Test of read profile from time evolution data file'
+    
+    inquire(file='structure_evolution.dat', exist=isDefined)
+    
+    if (.not.isDefined) then
+      write(Error_unit, *) "Warning: The file 'structure_evolution.dat' doesn't exist."
+      write(Error_unit, *) "A default one is being generated"
+      
+      open(10, file='structure_evolution.dat')
+      write(10,'(a)') '! time    log(Av)    log(n)    log(T)   '
+      write(10,'(a)') '! (Myr)   (mag)      (cm-3)    (K)      '
+      write(10,'(a)') '0.000e+00 -1.231e+00 1.813e+00 1.698e+00'
+      write(10,'(a)') '5.000e+00 -1.221e+00 1.760e+00 1.715e+00'
+      write(10,'(a)') '1.000e+01 -1.511e+00 1.327e+00 1.946e+00'
+      write(10,'(a)') '1.500e+01 -1.617e+00 1.166e+00 2.063e+00'
+      write(10,'(a)') '2.000e+01 -1.906e+00 7.300e-01 2.463e+00'
+      write(10,'(a)') '2.500e+01 -1.781e+00 8.800e-01 2.340e+00'
+      write(10,'(a)') '3.000e+01 -1.448e+00 1.367e+00 1.930e+00'
+      write(10,'(a)') '3.500e+01 -1.091e+00 1.909e+00 1.656e+00'
+      write(10,'(a)') '4.000e+01 -1.202e+00 1.748e+00 1.719e+00'
+      write(10,'(a)') '4.500e+01 -1.307e+00 1.583e+00 1.805e+00'
+      write(10,'(a)') '5.000e+01 -7.650e-01 2.432e+00 1.480e+00'
+      write(10,'(a)') '5.500e+01 -1.024e+00 2.058e+00 1.594e+00'
+      close(10)
+      
+    end if
+    
+    call init_structure_evolution()
+    
+    open(10, file='test_time_read.dat')
+    write(10,*) '# time (Myr), av [mag], density [part/cm^3], gas_temperature (K), grain_temperature (K)'
+    do j=1,structure_sample
+      
+      write(10,*) structure_time(j)/(1e6*YEAR), 10.d0**(structure_log_Av(j)), 10.d0**(structure_log_density(j)), &
+                  10.d0**(structure_log_gas_temperature(j)), 10.d0**(structure_log_dust_temperature(j))
+      ! if the grain temperature is not defined in the structure_evolution.dat file, grain temperature will appear to be 1K 
+      !! since the log will be set to 0
+    end do
+    close(10)
+    
+    ! We create associated gnuplot files
+    open(10, file="test_av_read.gnuplot")
+    open(11, file="test_density_read.gnuplot")
+    open(12, file="test_gas_temperature_read.gnuplot")
+    open(13, file="test_grain_temperature_read.gnuplot")
+    
+    do j=10,13
+      write(j,*) "set terminal pdfcairo enhanced"
+    end do
+    
+    write(10,*) '!rm "test_av_read.pdf"'
+    write(10,*) "set output 'test_av_read.pdf'"
+    write(10,*) 'set ylabel "Visual extinction [mag]"'
+    
+    write(11,*) '!rm "test_density_read.pdf"'
+    write(11,*) "set output 'test_density_read.pdf'"
+    write(11,*) 'set ylabel "Density [part/cm^3]"'
+    write(11,*) 'set logscale y'
+    
+    write(12,*) '!rm "test_gas_temperature_read.pdf"'
+    write(12,*) "set output 'test_gas_temperature_read.pdf'"
+    write(12,*) 'set ylabel "Temperature [K]"'
+    write(12,*) 'set logscale y'
+    
+    write(13,*) '!rm "test_grain_temperature_read.pdf"'
+    write(13,*) "set output 'test_grain_temperature_read.pdf'"
+    write(13,*) 'set ylabel "Temperature [K]"'
+    write(13,*) 'set logscale y'
+    
+    do j=10,13
+      write(j,*) 'set xlabel "Time [Myr]"'
+      write(j,*) 'set grid'
+      write(j,*) 'set logscale x'
+    end do
+
+    write(10,*) 'plot "test_time_read.dat" using 1:2 with points linetype 1 pointtype 2 title "Read profile",\'
+    write(10,*) '     "structure_evolution.dat" using 1:(10**$2) with lines title "Profile"'
+
+    write(11,*) 'plot "test_time_read.dat" using 1:3 with points linetype 1 pointtype 2 title "Read profile",\'
+    write(11,*) '     "structure_evolution.dat" using 1:(10**$3) with lines title "Profile"'
+
+    write(12,*) 'plot "test_time_read.dat" using 1:4 with points linetype 1 pointtype 2 title "Read profile",\'
+    write(12,*) '     "structure_evolution.dat" using 1:(10**$4) with lines title "Profile"'
+
+    select case(GRAIN_TEMPERATURE_TYPE)
+    case('gas')
+      write(13,*) 'plot "test_time_read.dat" using 1:5 with points linetype 1 pointtype 2 title "Read profile",\'
+      write(13,*) '     "structure_evolution.dat" using 1:(10**$4) with lines title "Profile"'
+    
+    case('table')
+      write(13,*) 'plot "test_time_read.dat" using 1:5 with points linetype 1 pointtype 2 title "Read profile",\'
+      write(13,*) '     "structure_evolution.dat" using 1:(10**$5) with lines title "Profile"'
+
+    case default
+      write(13,*) 'plot "test_time_read.dat" using 1:5 with points linetype 1 pointtype 2 title "Read profile"'
+    end select
+    
+    close(10)
+    close(11)
+    close(12)
+    close(13)
+  
+  end subroutine test_read_time_evolution
 
 end program unitary_tests
