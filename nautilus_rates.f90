cccbbedfd56efad3c895a6077233accd70533a8b
Christophe Cossou <ccossou@gmail.com>
2014-07-11 10:56:52 +0200

    correcting major bug in nautilus_rates that wasn't compiling anymore due to changes in other modules

--- nautilus_rates.f90
+++ nautilus_rates.f90
@@ -6,6 +6,14 @@ use utilities
 
 implicit none
 
+integer, parameter :: MAX_REACTANTS = 3 !< The maximum number of reactants for one reaction. 
+!! Do not think that changing this parameter alone is sufficient to allow the code to handle it directly !!
+integer, parameter :: MAX_PRODUCTS = 5 !< The maximum number of products for one reaction. 
+!! Do not think that changing this parameter alone is sufficient to allow the code to handle it directly !!
+integer, parameter :: MAX_COMPOUNDS = MAX_REACTANTS + MAX_PRODUCTS !< Total maximum number of compounds for one reaction (reactants + products)
+!! Warning: If this number change, get_jacobian(N, T, Y, J, IAN, JAN, PDJ) must be actualised, since each reactant and product has
+!! its own variable, a new one must be created for the new column possible. 
+
 ! Locals
 character(len=80) :: filename_output
 integer :: species, output, reaction ! index for loops
@@ -13,7 +21,7 @@ integer :: reactant1, reactant2, reactant3 ! to store indexes of the 3 possible 
 integer :: error ! to store the state of a read instruction
 logical :: isDefined
 
-integer :: nptmax = 1
+integer :: nb_sample_1D = 1
 
 character(len=80) :: rate_format, time_format !< string to store specific format used to output datas
 
@@ -31,15 +39,16 @@ integer :: nb_species !< the total number of species
 integer :: nb_reactions !< the total number of reactions
 
 real(double_precision), dimension(:), allocatable :: time
-real(double_precision), dimension(:,:), allocatable :: temperature
+real(double_precision), dimension(:,:), allocatable :: gas_temperature
+real(double_precision), dimension(:,:), allocatable :: dust_temperature
 real(double_precision), dimension(:,:), allocatable :: density
 real(double_precision), dimension(:,:), allocatable :: visual_extinction !< visual extinction
 real(double_precision), dimension(:,:), allocatable :: zeta
 
 ! For rates
 real(double_precision), allocatable, dimension(:,:) :: reaction_rates ! (nb_outputs, nb_reactions)
-character(len=11), allocatable, dimension(:,:) :: SYMBOL
-integer, allocatable, dimension(:) :: NUM !< index of the reactions (one of the columns of the concerned file, 
+character(len=11), allocatable, dimension(:,:) :: REACTION_COMPOUNDS_NAMES
+integer, allocatable, dimension(:) :: REACTION_ID !< index of the reactions (one of the columns of the concerned file, 
 !! declaring a given number for each reaction, like a hashtag.
 integer, allocatable, dimension(:,:) :: REACTION_COMPOUNDS_ID
 
@@ -67,7 +76,7 @@ call get_linenumber(filename='grain_reactions.in', nb_lines=nb_surface_reactions
 nb_species = nb_species_for_gas + nb_species_for_grain ! The total number of species, sum of species in gas and grain
 nb_reactions = nb_gas_phase_reactions + nb_surface_reactions ! The total number of reactions, sum of species in gas and grain
 
-write(*,'(a, i5)') 'Spatial resolution: ', nptmax
+write(*,'(a, i5)') 'Spatial resolution: ', nb_sample_1D
 write(*,'(a, i5)') 'Number of time outputs: ', nb_outputs
 write(*,'(a, i5)') 'Number of species: ', nb_species
 write(*,'(a, i6)') 'Number of reactions: ', nb_reactions
@@ -76,12 +85,13 @@ write(*,'(a, i6)') 'Number of reactions: ', nb_reactions
 allocate(species_name(nb_species))
 
 allocate(time(nb_outputs))
-allocate(temperature(nptmax, nb_outputs))
-allocate(density(nptmax, nb_outputs))
-allocate(visual_extinction(nptmax, nb_outputs))
-allocate(zeta(nptmax, nb_outputs))
+allocate(gas_temperature(nb_sample_1D, nb_outputs))
+allocate(dust_temperature(nb_sample_1D, nb_outputs))
+allocate(density(nb_sample_1D, nb_outputs))
+allocate(visual_extinction(nb_sample_1D, nb_outputs))
+allocate(zeta(nb_sample_1D, nb_outputs))
 
-allocate(abundances(nb_outputs, nb_species+1, nptmax)) ! We create an extra species that will always have an abundance of 1
+allocate(abundances(nb_outputs, nb_species+1, nb_sample_1D)) ! We create an extra species that will always have an abundance of 1
 
 allocate(REACTION_COMPOUNDS_NAMES(MAX_COMPOUNDS,nb_reactions))
 allocate(reaction_rates(nb_outputs, nb_reactions))
@@ -98,9 +108,10 @@ do output=1,nb_outputs
   write(filename_output, '(a,i0.6,a)') 'abundances.',output,'.out'
 
   open(10, file=filename_output, status='old', form='unformatted')
-  read(10) time(output), species_name(1:nb_species)
-  read(10) temperature(1:nptmax, output), density(1:nptmax, output), visual_extinction(1:nptmax, output), zeta(1:nptmax, output)
-  read(10) abundances(output,1:nb_species, 1:nptmax)
+  read(10) time(output)
+  read(10) gas_temperature(1:nb_sample_1D, output), dust_temperature(1:nb_sample_1D, output), density(1:nb_sample_1D, output), &
+           visual_extinction(1:nb_sample_1D, output), zeta(1:nb_sample_1D, output)
+  read(10) abundances(output,1:nb_species, 1:nb_sample_1D)
   close(10)
   
   write(filename_output, '(a,i0.6,a)') 'rates.',output,'.out'
@@ -117,7 +128,7 @@ write(*,'(a,a)') achar(13), 'Reading unformatted outputs... Done'
 
 ! For non existing reactants in reactions, we create a new species whose abundance is always 1, so that we can calculate the fluxes 
 !! more easily.
-abundances(1:nb_outputs, nb_species+1, 1:nptmax) = 1.d0
+abundances(1:nb_outputs, nb_species+1, 1:nb_sample_1D) = 1.d0
 
 ! We retrieve indexes of each reactants for each reaction
 call set_only_reactants()
@@ -146,14 +157,14 @@ enddo
 ! The next write will be written in the same line
 write(*,'(a)', advance='no') 'Writing rates ASCII files...'
 
-write(rate_format, '(a,i2,a,i5,a)') '(',MAX_COMPOUNDS,'(a11," "),', nb_outputs, '(es12.4e2),i5)'
-write(time_format, '(a,i5,a)') '(73(" "),a11,', nb_outputs, '(es12.4e2))'
+write(rate_format, '(a,i0,a,i0,a)') '(',MAX_COMPOUNDS,'(a11," "),', nb_outputs, '(es12.4e2),i5)'
+write(time_format, '(a,i0,a,i0,a)') '(',12*(MAX_COMPOUNDS-1),'(" "),a12,', nb_outputs, '(es12.4e2))'
 
 ! We write ASCII output file
 open(10, file='rates.out')
 ! all MAX_COMPOUNDS species involved ('XXX' if no species) ; Each column is the flux for several output times'
 ! The first line list time for each column of flux
-write(10, time_format) ' Time (yr)', time(1:nb_outputs)
+write(10, time_format) 'Time (year)', time(1:nb_outputs)
 do reaction=1, nb_reactions
   write(10,rate_format) REACTION_COMPOUNDS_NAMES(1:MAX_COMPOUNDS, reaction), reaction_fluxes(1:nb_outputs, reaction), &
                         REACTION_ID(reaction)
