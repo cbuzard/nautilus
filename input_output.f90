160407a80718fae31e1db09ba8067ee3139f5974
Christophe Cossou <ccossou@gmail.com>
2014-07-30 14:20:36 +0200

    Correcting a 'bug' in input_outputs. A factor of two for the density has been corrected. Now, throughout the code, the only density we have is in particule/cm^3. We don't speak of proton density anymore. A paragraph in the documentation will be added soon.

--- input_output.f90
+++ input_output.f90
@@ -1038,7 +1038,7 @@ open(UNIT=35, file=filename_output, form='unformatted')
 
 write(35) current_time
 write(35) gas_temperature(1:nb_sample_1D), dust_temperature(1:nb_sample_1D), &
-          0.5d0 * H_number_density(1:nb_sample_1D), visual_extinction(1:nb_sample_1D), X_IONISATION_RATE
+          H_number_density(1:nb_sample_1D), visual_extinction(1:nb_sample_1D), X_IONISATION_RATE
 write(35) abundances(1:nb_species, 1:nb_sample_1D)
 close(35)
 
@@ -1110,7 +1110,7 @@ open(13, file=filename)
 
 ! Header
 write(13,'(5(a,es10.3e2),a)') '!time =', current_time, ' s ; density = ', H_number_density, &
-' part/cm-3 ; temperature=', gas_temperature,' K ; visual extinction = ', visual_extinction, &
+' part/cm^3 ; temperature=', gas_temperature,' K ; visual extinction = ', visual_extinction, &
 ' [mag] ; CR ionisation rate = ',CR_IONISATION_RATE,' s-1'
 
 ! To adapt the format in function of the 1D number of points
